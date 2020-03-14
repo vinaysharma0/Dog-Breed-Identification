925f88d8a3757dc444b0bd59b890578e4b9b0fe8
vinswalker <svinay0488@gmail.com>
2020-03-14 16:35:10 +0530

    completed with Tk & Ipy run

--- breed_iden.py
+++ breed_iden.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
-from os.path import join # exists , expanduser
-#from tqdm import tqdm
-#from sklearn.metrics import log_loss , accuracy_score
+from os.path import join
+
 from keras.preprocessing import image
+import keras
 #from tensorflow.keras.applications import ResNet50
 from os import listdir
-import matplotlib.pyplot as plt
 from keras.applications.vgg16 import  preprocess_input, decode_predictions
+
 import cv2
 
+#.......................Remove all the comments and comment all the code which is not commented except the modules if you want to train your own model and then test it on the dataset...................#
+
+
+"""
 #....................................................
 input_size = 224
 num_of_class = 15
@@ -27,27 +31,28 @@ labels = labels[labels['breed'].isin(selected_breed_list)]
 labels['target'] = 1
 labels['rank'] = labels.groupby('breed').rank()
 labels_pivot = labels.pivot('id', 'breed', 'target').reset_index().fillna(0)
-
+"""
+"""
 def read_img(img_id, train_or_test, size):
     img =cv2.imread(join(data_dir, train_or_test, img_id + '.jpg'))
     img = cv2.resize(img,size)
-   # img = image.img_to_array(img)
-
     return img
-#read_img('000bec180eb18c7604dcecc8fe0dba07','train',(224,224))
+"""
 
 #predictions
 
-
 from sklearn.utils import shuffle
-import tensorflow.keras.applications.inception_v3 as inception_v3
-model = inception_v3.InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling='max', classes=1000)
+from tensorflow.keras.applications import NASNetLarge
+model = NASNetLarge(weights='imagenet')
+
+# Lower loop can be used if you want to test the data on your Algorithm.
+"""
 true= []
-def Run_model():
+def Run_model(num_of_img):
     i = 0
-    for  img_id, breed,_,_ in shuffle(labels).tail(5).itertuples(index=False):
+    for  img_id, breed,_,_ in shuffle(labels).head(num_of_img).itertuples(index=False):
         i += 1
-        img = read_img(img_id, 'train_set/train',(299,299))    
+        img = read_img(img_id, 'train_set/train',(331,331))    
         x = preprocess_input(np.expand_dims(img.copy(), axis=0))
         x = x / 255
         preds = model.predict(x)
@@ -59,97 +64,85 @@ def Run_model():
         print(i, '  predicted_breed : ' + imagenet_class_name ,"||", 'original_breed : ' + breed)
        
     plt.show()
-
-
-#using GPU
+"""
 import tensorflow as tf
 assert tf.test.is_gpu_available()
 assert tf.test.is_built_with_cuda()
 
-Run_model()
-#Running the model
+config = tf.compat.v1.ConfigProto( device_count = {'GPU': 1 , 'CPU': 8} ) 
+sess = tf.compat.v1.Session(config=config) 
+keras.backend.set_session(sess)
 
-"""
-#making application
+
+# importing only those functions 
 from tkinter import *
-
+  
 # loading Python Imaging Library 
 from PIL import ImageTk, Image 
-
-# To get the dialog box to open when required 
+  
+# To get the dialog box to open when required  
 from tkinter import filedialog 
+
 def openfilename(): 
 
 	# open file dialog box to select image 
 	# The dialogue box has a title "Open" 
-	filename = filedialog.askopenfilename() 
+	filename = filedialog.askopenfilename(title ='Open') 
 	return filename 
-def open_img(): 
-    # Select the Imagename  from a folder  
-    x = openfilename() 
-  
-    # opens the image 
-    img = Image.open(x) 
-      
-    # resize the image and apply a high-quality down sampling filter 
-    img = img.resize((250, 250), Image.ANTIALIAS) 
-  
-    # PhotoImage class is used to add image to widgets, icons etc 
-    img = ImageTk.PhotoImage(img) 
-   
-    # create a label 
-    panel = Label(root, image = img) 
-      
-    # set the image as img  
-    panel.image = img 
-    panel.grid(row = 2) 
+def open_img():
+    x = openfilename()
+    img = Image.open(x)
+    img = img.resize((331,331), Image.ANTIALIAS)
+    y = preprocess_input(np.expand_dims(img, axis=0))
+    y = y / 225
+    pred = model.predict(y)
+    _, pred_name, prob = decode_predictions(pred,top=0)[0][0]
+    print(pred_name)
+    img = ImageTk.PhotoImage(img)
+    #img = image.load_img(img)
     
-    def read_img(img):
-        img =cv2.imread(img)
-        img = cv2.resize(img,size)
-        img = image.img_to_array(img)
-
-    return img
     
-    from sklearn.utils import shuffle
-    import tensorflow.keras.applications.inception_v3 as inception_v3
-    model = inception_v3.InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling='max', classes=1000)
-    true= []
-    def Run_model():
-        i = 0
-        for  img_id, breed,_,_ in shuffle(labels).itertuples(index=False):
-            i += 1
-            img = read_img(img_id, 'train_set/train',(299,299))    
-            x = preprocess_input(np.expand_dims(img.copy(), axis=0))
-            x = x/255
-            preds = model.predict(x)
-            _, imagenet_class_name, prob = decode_predictions(preds, top=0)[0][0]
-            #cv2.putText(img,imagenet_class_name,(10,10),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(255, 0, 0),thickness=1,bottomLeftOrigin = False)
-            cv2.putText(img, breed,(10,10),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(255,0,0),thickness=1,bottomLeftOrigin = False)
-            #cv2.imshow(imagenet_class_name,img)
-            true.append(imagenet_class_name.lower() == breed)
-            print(i, '  predicted_breed : ' + imagenet_class_name ,"||", 'original_breed : ' + breed)
-           
-        plt.show()
+    #img = read_img(img)
+    panel = Label(root,image = img)
+    panel.image = img
+    panel.grid(row = 3) 
+    text = StringVar(root)
+    text.set(pred_name)
+    l = Label(root, textvariable=text, font=("comic sans ms", 10))
+    l.place(x=400,y=90)
+    
+    
 
 
-    
-    Label(root, text = 'dummy' ).place(x = 400,y = 250)  
-    
 # Create a windoe 
 root = Tk() 
-
+  
 # Set Title as Image Loader 
 root.title("Image Loader") 
-#text...........................
-
+  
 # Set the resolution of window 
-root.geometry("600x600") 
+root.geometry("550x300") 
 
 # Allow Window to be resizable 
 root.resizable(width = True, height = True) 
-
+  
 # Create a button and place it into the window using grid layout 
-btn = Button(root, text ='open image', command = open_img).grid(row = 1, columnspan = 4) 
+btn = Button(root, text ='open image', command = open_img).grid( 
+                                        row = 1, columnspan = 4) 
 root.mainloop() 
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
